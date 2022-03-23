6d1de6763d6c4c576acf0e0249f425a779e450c1
Israel Dillanes <israeldillanes2@gmail.com>
2022-03-23 13:25:49 -0600

    Diseño para celular

---
+++ js/cod.js
@@ -0,0 +1,8 @@
+let oneClick = document.querySelector(".menu-icon");
+let menu = document.querySelector(".menu");
+
+oneClick.addEventListener(`click`,()=>{
+  menu.classList.toggle("show");
+});
+
+
